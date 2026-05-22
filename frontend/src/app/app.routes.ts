63d8cfe0df23335dc7d22fb8deff2bfa2ac641e7
Emanuel <emanuelcomas96@gmail.com>
2026-05-22 15:59:16 -0300

    Agregado de frontend de Clientes.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Login } from './auth/login/login';
 import { TareasListado } from './proyectos/tareas/listado/tareas-listado';
 import { ProyectosListado } from './proyectos/listado/proyectos-listado';
+import { ClientesListado } from './proyectos/clientes/listado/clientes-listado';
 
 export const routes: Routes = [
     {
@@ -15,6 +16,10 @@ export const routes: Routes = [
     {
         path: 'proyectos',
         component: ProyectosListado
+    },
+    {
+        path: 'clientes',
+        component: ClientesListado
     },
     {
         // Si no coincide ninguna ruta, redirigiar a 'login'.
